8b4a01ae2e5764357cc249415d5ad3aae2939aa8
Muhammad Nauman <naumancs@gmail.com>
2021-01-25 01:06:28 +0500

    added

--- compiled.py
+++ compiled.py
@@ -1,12 +1,12 @@
 import json
 
 compiled = [];
-for index in ['4', '5', '7', '8', '9']:
-	f = open("processed/processed" + index + ".json")
+for index in ['1', '2', '3']:
+	f = open("data/" + index + ".json")
 	data = json.load(f)
 	compiled = compiled + data
 
 print(len(compiled))
 
-with open('compiled/compiled.json', 'w') as outfile:
+with open('compiled/compiled_f.json', 'w') as outfile:
 	json.dump(compiled, outfile)
---
+++ shapely.py
@@ -0,0 +1,9 @@
+import json
+import shapely
+
+compiled = open("compiled/compiled.json")
+coords = json.load(compiled)
+d = open('doctors.json')
+doctors = json.load(d)
+print(shapely)
+exit()
