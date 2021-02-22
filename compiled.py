8a4452e9acdf37de6f693ef15ff9d83fdbb8768a
Muhammad Nauman <naumancs@gmail.com>
2021-02-22 16:12:58 +0500

    wip

--- compiled.py
+++ compiled.py
@@ -2,11 +2,11 @@ import json
 
 compiled = [];
 for index in ['1', '2', '3']:
-	f = open("data/" + index + ".json")
+	f = open("processed/processed" + index + ".json")
 	data = json.load(f)
 	compiled = compiled + data
 
 print(len(compiled))
 
-with open('compiled/compiled_f.json', 'w') as outfile:
+with open('compiled/compiled_p.json', 'w') as outfile:
 	json.dump(compiled, outfile)
--- python3.py
+++ python3.py
@@ -1,24 +1,47 @@
 import geocoder
 import json
+import requests
 
 
 
 coords = []
 counter = 1;
+processedCounter = 1;
 
-f = open("data/coords3.json")
+f = open("compiled/compiled_f.json")
 data = json.load(f)
 for coord in data:
 	print(counter)
-	g = geocoder.arcgis([coord[0]['lat'], coord[0]['lng']], method='reverse')
-	if g.country == 'PAK':
-		details = {
-			"city": g.city,
-			"state": g.state,
-			"state_long": g.state_long
-		}
-		coord.append(details)
-		coords.append(coord)
+	if counter > 14000:
+		if(coord['count'] > 0):
+			batch = []
+			for c in coord['coords']:
+				batch.append({
+					"query": str(c['lat']) + ',' + str(c['lng']),
+					"limit": 1,
+				})
+			params = { "batch": batch }
+			# print(json = params)
+			r = requests.post('http://api.positionstack.com/v1/reverse?access_key=b45fcf9ef72b16ec609e755cf87a5187', json=params)
+			# g = geocoder.arcgis([coord['coords'][0]['lat'], coord['coords'][0]['lng']], method='reverse')
+			response = r.json()
+			# if g.country == 'PAK':
+			# print(response)
+			g = response['data']
+			details = []
+			for res in g:
+				details.append({
+					"place": res['name'],
+					"province": res['region'],
+					"county": res['county']
+				})
+			coord['details'] = details;
+			coords.append(coord)
+			print("processedCounter" + str(processedCounter))
+			processedCounter += 1
+		else:
+			coord['details'] = []
+			coords.append(coord)
 	counter += 1
 with open('processed/processed3.json', 'w') as outfile:
     json.dump(coords, outfile)
